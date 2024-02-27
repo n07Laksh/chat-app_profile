d607c426e09010bf1699bf8ba8da72c50b34fc7e
laxmi <dlaksh74@gmail.com>
2024-02-27 20:36:22 +0530

    cors policy update

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(
+    cors({
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
 dbconnection();
 
 app.get("/",(req,res)=>{
