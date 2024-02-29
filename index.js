f7d7f347749eef51f287651abc2c92f751e7ea55
laxmi <dlaksh74@gmail.com>
2024-02-29 09:23:10 +0530

    cors origin update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: "https://next-chat-7.vercel.app",
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     })
